36e3051db4a2346b8ea576aa33f6d8f4c9a55af1
Hussam Alghamdi <hussam@example.com>
2026-05-07 07:16:26 +0300

    Initialize game logic and state variables for Tic Tac Toe

---
+++ script.js
@@ -0,0 +1,17 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#turns");
+const restartButton = document.querySelector("#restart-btn");
+const winsConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let playerTurn = "X";
+let running = true;
+
